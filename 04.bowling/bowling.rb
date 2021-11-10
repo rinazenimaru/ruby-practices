30a3b7e271745561e708deb373b024d279a74762
rinazenimaru <rina.zenimaru@icare.jpn.com>
2021-11-10 18:45:52 +0900

    ボウリングの課題の修正を提出

---
+++ 04.bowling/bowling.rb
@@ -0,0 +1,61 @@
+score = ARGV[0]
+scores = score.split(',')
+frames = []
+frame = []
+max_scores_size = scores.size
+
+scores.each.with_index(1) do |s, index|
+  if s == 'X'
+    frame << 10
+    if frames.size != 9
+      frames << frame
+      frame = []
+    else
+      if index == max_scores_size
+        frames << frame
+      end
+    end
+  else
+    frame << s.to_i
+    if frame.size == 2 && frames.size != 9
+      frames << frame
+      frame = []
+    else
+      if (frame.size == 2 && frame.sum < 10) || (frame.size == 3)
+          frames << frame
+          break
+      end
+    end
+  end
+end
+
+total_score = 0
+
+frames.each_with_index do |current_frame, index|
+  next_frame = frames[index + 1]
+  next_next_frame = frames[index + 2]
+  total_score += current_frame.sum
+
+  # 普通/スペア/ストライク
+  if current_frame.sum < 10 
+    # 普通
+
+  elsif current_frame.sum == 10 && current_frame.size == 2
+    # スペア
+    total_score += next_frame[0] 
+
+  else
+    if current_frame.sum == 10 && current_frame.size == 1
+      # ストライク
+      total_score += next_frame[0]
+
+      if !next_frame.nil? && next_frame.size > 1
+        total_score += next_frame[1]
+      elsif !next_frame.nil? && next_frame.size == 1
+        total_score += next_next_frame[0]
+      end
+    end
+  end
+end
+
+p total_score
