69923b049c3803329c394ce9a765b068faabbd1c
rinazenimaru <rina.zenimaru@icare.jpn.com>
2021-11-19 19:59:09 +0900

    ボウリングの課題の修正を再提出

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -2,7 +2,6 @@ score = ARGV[0]
 scores = score.split(',')
 frames = []
 frame = []
-max_scores_size = scores.size
 
 scores.each.with_index(1) do |s, index|
   if s == 'X'
@@ -11,7 +10,7 @@ scores.each.with_index(1) do |s, index|
       frames << frame
       frame = []
     else
-      if index == max_scores_size
+      if index == scores_size
         frames << frame
       end
     end
@@ -36,17 +35,13 @@ frames.each_with_index do |current_frame, index|
   next_next_frame = frames[index + 2]
   total_score += current_frame.sum
 
-  # 普通/スペア/ストライク
-  if current_frame.sum < 10 
-    # 普通
+  current_frame.sum < 10 
 
-  elsif current_frame.sum == 10 && current_frame.size == 2
-    # スペア
+  if current_frame.sum == 10 && current_frame.size == 2
     total_score += next_frame[0] 
 
   else
     if current_frame.sum == 10 && current_frame.size == 1
-      # ストライク
       total_score += next_frame[0]
 
       if !next_frame.nil? && next_frame.size > 1
